f6201e0e1a571e56b3ff64718e6a8bc822dde1dc
Eiden <eidnegabriel07@gmail.com>
2022-06-14 10:18:50 -0400

    fix task 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -2,15 +2,15 @@
 
 /**
  * swap - swap item using pointers
- *@xp: first item
- *@yp: second item
+ *@a: first item
+ *@b: second item
  *
  */
-void swap(int *xp, int *yp)
+void swap(int *a, int *b) 
 {
-	int temp = *xp;
-	*xp = *yp;
-	*yp = temp;
+  int temp = *a;
+  *a = *b;
+  *b = temp;
 }
 
 /**
@@ -23,17 +23,17 @@ void swap(int *xp, int *yp)
  */
 void selection_sort(int *array, size_t size)
 {
-	unsigned int i, j, min_idx;
-
-	for (i = 0; i < size - 1; i++)
-	{
-		min_idx = i;
-		for (j = i + 1; j < size; j++)
-		{
-			if (array[j] < array[min_idx])
-				min_idx = j;
-		}
-		swap(&array[min_idx], &array[i]);
-		print_array(array, size);
-	}
+   unsigned int i, j, min;
+  
+    for (i = 0; i < size - 1; i++)
+    {
+        min = i;
+        for (j = i + 1; j < size; j++)
+        {
+          if (array[j] < array[min])
+            min = j;
+        }
+        swap(&array[min], &array[i]);
+        print_array(array, size);
+    }
 }
