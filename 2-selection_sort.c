5d5934491a22be61291dc73227d45c14fab0835b
Eiden <eidnegabriel07@gmail.com>
2022-06-14 10:21:02 -0400

    fix task 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -6,11 +6,11 @@
  *@b: second item
  *
  */
-void swap(int *a, int *b) 
+void swap(int *a, int *b)
 {
-  int temp = *a;
-  *a = *b;
-  *b = temp;
+	int temp = *a;
+	*a = *b;
+	*b = temp;
 }
 
 /**
@@ -23,17 +23,17 @@ void swap(int *a, int *b)
  */
 void selection_sort(int *array, size_t size)
 {
-   unsigned int i, j, min;
-  
-    for (i = 0; i < size - 1; i++)
-    {
-        min = i;
-        for (j = i + 1; j < size; j++)
-        {
-          if (array[j] < array[min])
-            min = j;
-        }
-        swap(&array[min], &array[i]);
-        print_array(array, size);
-    }
+	unsigned int i, j, min;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min])
+				min = j;
+		}
+		swap(&array[min], &array[i]);
+		print_array(array, size);
+	}
 }
